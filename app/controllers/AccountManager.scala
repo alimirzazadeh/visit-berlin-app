1f2ecb22c35d18b6830259462fb7483fbf984e6e
Tyler <tscaram@gmail.com>
2019-06-12 14:21:11 +0200

    finished read and write to csv methods

--- app/controllers/AccountManager.scala
+++ app/controllers/AccountManager.scala
@@ -15,22 +15,30 @@ class AccountManager {
       else
         accList
     }
-    createList(List(), Source.fromFile(AccountManager.filename).getLines.drop(1))
+    val file = new File(AccountManager.filename)
+    if (file.exists())
+      createList(List(), Source.fromFile(AccountManager.filename).getLines.drop(1))
+    else {
+      writeToCSV(List())
+      List()
+    }
   }
 
   def writeToCSV(accList: List[Account]): Unit = {
     val writer = new PrintWriter(new File(AccountManager.filename))
     writer.write("firstname,lastname,email,username,password\n")
     for (account <- accList) {
-      val current = account.makeStringList()          //ask nick about this line - why is there a parameter
+      val current = account.makeStringList
       for (item <- 0 to 3) writer.write(current(item) + ",")
       writer.write(current(4) + '\n')
     }
+    writer.close()
   }
 
   def addAccount(acc: Account): List[Account] = {
     val accList = readFromCSV
-    val updatedList = if (accList.contains(acc)) accList else acc :: accList
+    val updatedList = if (accList.contains(acc)) accList else acc :: accList //this doesn't work - duplicates are still added
+    //doesn't work because the string version of password hash is different each time -- look at target/accounts.csv
     updatedList
   }
 
@@ -48,12 +56,17 @@ class AccountManager {
 }
 
 object AccountManager {
-  val filename: String = "public/accounts.csv"
+  val filename: String = "accounts.csv"
   
   // Run this main method to manually test Account and AccountManager instances and functions
   // If readFromCSV is not implemented at time of test, a dummy return must be provided for it
   def main(args: Array[String]) = {
     val am1 = new AccountManager
-    
+    am1.writeToCSV(am1.addAccount(Account("TYLER","SCAR","tscaram@gmail.com","tscaram","test2")))
+    am1.writeToCSV(am1.addAccount(Account("TYLER","SCAR","tscaram@gmail.com","tscaram","test2")))
+    am1.writeToCSV(am1.addAccount(Account("TYLER","SCAR","tscaram@gmail.com","tscaram","test2")))
+
+    //am1.writeToCSV(am1.removeAccount(Account("TYLER","SCAR","tscaram@gmail.com","tscaram","test2")))
+    //looks like add, remove, and edit don't work because the string version of the hash is random in the csv
   }
 }
