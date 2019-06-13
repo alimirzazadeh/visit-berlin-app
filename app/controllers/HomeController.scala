82f76fa16e92a200ecfb9500a7e1cf94f9412a9e
Nick Poulos <npoulos9825@gmail.com>
2019-06-13 12:58:55 +0200

    Fixed salting and camelCase in home controller

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -25,26 +25,26 @@ class HomeController @Inject()(cc: ControllerComponents)(implicit assetsFinder: 
   }
 
   def after = Action { implicit request =>
-    val newprofile = new Profile(request.body.asFormUrlEncoded.get("firstname").head, request.body.asFormUrlEncoded.get("lastname").head,
-      request.body.asFormUrlEncoded.get("birthyear").head.toInt, request.body.asFormUrlEncoded.get("hometown").head,
+    val newProfile = Profile(request.body.asFormUrlEncoded.get("firstName").head, request.body.asFormUrlEncoded.get("lastName").head,
+      request.body.asFormUrlEncoded.get("birthYear").head.toInt, request.body.asFormUrlEncoded.get("hometown").head,
       request.body.asFormUrlEncoded.get("interests").head)
-    val newaccount = new Account(request.body.asFormUrlEncoded.get("email").head, request.body.asFormUrlEncoded.get("password").head, "idk", newprofile, false)
+    val newAccount = new Account(request.body.asFormUrlEncoded.get("email").head, request.body.asFormUrlEncoded.get("password").head, Account.generateSalt(), newProfile, false)
     //AccountManager.addAccount(newaccount);
     val am = new AccountManager
-    am.writeToCSV(am.addAccount(newaccount))
+    am.writeToCSV(am.addAccount(newAccount))
     //
-    Ok(views.html.after(newaccount))
+    Ok(views.html.after(newAccount))
   }
 
   val userForm = Form(
     mapping(
-      "firstname" -> text,
-      "lastname"  -> text,
+      "firstName" -> text,
+      "lastName"  -> text,
       "email" -> text,
       "hometown" -> text,
       "interests" -> text,
       "password" -> text,
-      "birthyear" -> number
+      "birthYear" -> number
     )(UserData.apply)(UserData.unapply)
   )
 //  def formstuff = Action {
